ddf06a23253cfb501c4b2907da8cd17b5ac72a95
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-07 14:48:42 -0500

    add node types - npm i @aws-cdk/aws-lambda @aws-cdk/core

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,9 +1,12 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Function as LambdaFunction } from "aws-cdk-lib/lib/aws-lambda";
+import { join } from "path";
 
 export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {});
   }
 }
