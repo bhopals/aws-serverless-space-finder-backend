4684c7a296c99cb544671aec4cced20e347c5a87
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-07 15:17:53 -0500

    AWS CDK - Add api Gateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -6,9 +6,13 @@ import {
   Function as LambdaFunction,
 } from "aws-cdk-lib/lib/aws-lambda";
 
+import { RestApi, LambdaIntegration } from "aws-cdk-lib/lib/aws-apigateway";
+
 import { join } from "path";
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
@@ -17,5 +21,10 @@ export class SpaceStack extends Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.handler",
     });
+
+    //Hello Api Lambda Integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello')
+    helloLambdaResource;.addMethod('GET', helloLambdaIntegration)
   }
 }
