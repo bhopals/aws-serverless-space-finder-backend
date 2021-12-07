f40d5445feced712218fd87dcac193840b2f7ee1
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-07 15:10:38 -0500

    add node types - add Lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,12 +1,21 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Function as LambdaFunction } from "aws-cdk-lib/lib/aws-lambda";
+import {
+  Code,
+  Runtime,
+  Function as LambdaFunction,
+} from "aws-cdk-lib/lib/aws-lambda";
+
 import { join } from "path";
 
 export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {});
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.handler",
+    });
   }
 }
