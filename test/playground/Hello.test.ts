e6b384d1b3e929cf383da74529724e348ee44f87
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-10 06:47:23 -0500

    Adding logs

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,4 +1,4 @@
 import { handler } from "../../services/node-lambda/hello";
-console.log("Handler Test Finish...");
+console.log("Handler  Test Finish...");
 handler({}, {});
-console.log("Handler Test Finish...");
+console.log("Handler  Test Finish...");
