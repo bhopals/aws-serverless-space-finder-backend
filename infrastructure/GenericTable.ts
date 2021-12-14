fb717e2c5712a4bb61ff7c2963c6a1b2ebb63111
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-14 17:55:25 -0500

    Dynamo Lambda Integration

--- infrastructure/GenericTable.ts
+++ infrastructure/GenericTable.ts
@@ -1,11 +1,32 @@
 import { Stack } from "aws-cdk-lib";
 import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+
+export interface TableProps {
+  tableName: string;
+  primaryKey: string;
+  createLambdaPath?: string;
+  readLambdaPath?: string;
+  updateLambdaPath?: string;
+  deleteLambdaPath?: string;
+}
 
 export class GenericTable {
   private name: string;
   private primaryKey: string;
   private stack: Stack;
   private table: Table;
+
+  private createLambda: NodejsFunction | undefined;
+  private readLambda: NodejsFunction | undefined;
+  private updateLambda: NodejsFunction | undefined;
+  private deleteLambda: NodejsFunction | undefined;
+
+  public createLamabdaIntegration: LambdaIntegration;
+  public readLamabdaIntegration: LambdaIntegration;
+  public updateLamabdaIntegration: LambdaIntegration;
+  public deleteLamabdaIntegration: LambdaIntegration;
 
   public constructor(name: string, primaryKey: string, stack: Stack) {
     this.name = name;
