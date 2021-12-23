d2a0b4cd405d5d8dabf3ff15f2883660453410c1
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-23 18:20:18 -0500

    Securing API - Authentication Provider - Cognito

--- test/playground/Auth/Auth.test.ts
+++ test/playground/Auth/Auth.test.ts
@@ -1,9 +1,22 @@
 import { AuthService } from "./AuthService";
 import { config } from "./config";
+import * as AWS from "aws-sdk";
 
 const authService = new AuthService();
 
-const user = authService.login(
-  config.TEST_USER_NAME,
-  config.TEST_USER_PASSWORD
-);
+// const user = authService.login(
+//   config.TEST_USER_NAME,
+//   config.TEST_USER_PASSWORD
+// );
+
+async function callStuff() {
+  const authService = new AuthService();
+  const user = authService.login(
+    config.TEST_USER_NAME,
+    config.TEST_USER_PASSWORD
+  );
+  await authService.getAWSTemporaryCreds(user);
+  const someCreds = AWS.config.credentials;
+}
+
+callStuff();
